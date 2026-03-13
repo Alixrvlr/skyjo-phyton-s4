58a9c7429fc78aa15c0a48e80687ef9c9914a3ce
Alixrvlr <alix.reveillere@reseau.eseo.fr>
2026-03-13 11:49:19 +0100

    debut du jeu

--- jeu.py
+++ jeu.py
@@ -25,8 +25,29 @@ print(listeJ1)
 
 print("")
 listeJ1 = convertirJeuCartes3Tableau(listeJ1)
-listeJ2 = convertirJeuCartes3Tableau(listeJ1)
-listeJ3 = convertirJeuCartes3Tableau(listeJ1)
-listeJ4 = convertirJeuCartes3Tableau(listeJ1)
+listeJ2 = convertirJeuCartes3Tableau(listeJ2)
+listeJ3 = convertirJeuCartes3Tableau(listeJ3)
+listeJ4 = convertirJeuCartes3Tableau(listeJ4)
 
+gagner = False
 
+while not gagner :
+   joueur=1
+   for i in range (nbJoueur):
+        joueur +=1
+        position = 1            #recuperer l'endroit du clic
+        typeJeu = "piocher"          #recuperer ce que le joueuer veut faire en fonction d'ou il clic
+       
+        if typeJeu=="piocher" :
+            jouerCarte = True                        # fonction qui affiche un pop up pour montrer la carte et 2 boutons sil veut garder cette carte et qui renvoi oui ou non
+            if jouerCarte :
+                position = 2                           #recuperer l'endroit du clic
+                echangeCarte("listeJ"+str(joueur),"listeEtatCarteJ"+str(joueur),1,position)
+            pass
+      
+        if typeJeu=="defausse" :
+            pass
+    
+        if typeJeu=="retourneCarte" :
+            pass
+         
