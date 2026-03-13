fbf0f98b905e75850323ca9546afc6693fb92edb
Lise <lise.miecko@gmail.com>
2026-03-13 11:44:27 +0100

    Modification interface : détermination des coordonnées du plateau

--- interface.py
+++ interface.py
@@ -1,41 +1,65 @@
 from tkinter import *
 
+# Fichier Interface Graphique du jeu - projet S4 informatique
 
+''' Création de la fenêtre ----------------------------------------------------------------------------------------'''
 
 fenetre = Tk()
-fenetre.title("TP3 - Le morpion")
+fenetre.title("Skyjo")
+fenetre.iconbitmap("logo.ico")
+
+largeur = fenetre.winfo_screenwidth()
+hauteur = fenetre.winfo_screenheight()
+
+# la fenêtre prend tout l'écran
+fenetre.geometry(f"{largeur}x{hauteur}")
+
 
 """
 largeur = fenetre.winfo_screenwidth()
 hauteur = fenetre.winfo_screenheight()
 fenetre.geometry(f"{largeur}x{hauteur}")
- 
+
 # Canvas principal
 canvas = tk.Canvas(fenetre, bg="white")
 canvas.pack(fill="both", expand=True)
-#fill="both" : prend largeur + hauteur
-#expand=True : utilise tout l’espace disponible
+fill="both" : prend largeur + hauteur
+expand=True : utilise tout l’espace disponible
 """
 
-can = Canvas(fenetre, width =1200, height =600, bg ='ivory')
-can.pack(side =TOP, padx =0, pady =0)
+''' Création du plateau du jeu ---------------------------------------------------------------------------------------'''
+
+# Délimitations du plateau de jeu 
+# width=fenetre.winfo_screenwidth() = 1366
+# height= 710
+
+# Délimitations d'un paquet de cartes de joueur
+# width=(largeur/2)-(largeur/10) = 550
+# height=((hauteur-60)/2)-70 = 290
+
+print(hauteur-60, fenetre.winfo_screenwidth())
+
+# test taille joueur
+can = Canvas(fenetre, bg ='black')
+can.place(anchor="nw", width=550, height=290, x=0, y=0)
 
 
-bQ = Button(fenetre, text ='Quitter', command = fenetre.destroy)
-bQ.pack(side =RIGHT, padx =3, pady =3)
 
-bR= Button(fenetre, text ='Rejouer')
-bR.pack(side =LEFT, padx =3, pady =3)
+
+
+
+
+''' Boutons Quitter/Rejouer ----------------------------------------------------------------------------------------'''
+
+bQuitter = Button(fenetre, text ='Quitter', command = fenetre.destroy)
+#bQuitter.grid(row=35, column=0, sticky="s", padx = 5, pady = 1)
+
+bRejouer= Button(fenetre, text ='Rejouer')
+#bRejouer.grid(row=35, column=5, sticky="s", padx = 5, pady = 1)
 
 
 #grille()
 #case = can.bind('<Button-1>', dessiner)
-
-
-
-
-
-
 
 
 
