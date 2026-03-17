f33d6c49db1da37497cacb9fc56921bee4e5aefc
Lise <lise.miecko@gmail.com>
2026-03-17 09:37:30 +0100

    Cases cartes terminées

--- interface.py
+++ interface.py
@@ -34,8 +34,7 @@ expand=True : utilise tout l’espace disponible
 # height= 710
 
 # Délimitations d'un paquet de cartes de joueur
-# width=(largeur/2)-(largeur/10) = 550
-# height=((hauteur-60)/2)-70 = 290
+# Espacement des cartes = 5  
 
 # Délimitation d'une carte 
 # width = 75
@@ -47,21 +46,75 @@ can = Canvas(fenetre, bg ='black')
 can.place(anchor="nw", width=fenetre.winfo_screenwidth(), height=710, x=0, y=0)
 
 # cartes J1 (haut gauche)
-can.create_rectangle(10, 10, 85, 115,width=1,fill="#808080") 
-can.create_rectangle(10, 120, 85, 225,width=1,fill="#808080") 
-can.create_rectangle(10, 230, 85, 335,width=1,fill="#808080") 
+can.create_rectangle(30, 10, 105, 115,width=1,fill="#808080") 
+can.create_rectangle(30, 120, 105, 225,width=1,fill="#808080") 
+can.create_rectangle(30, 230, 105, 335,width=1,fill="#808080") 
 
-can.create_rectangle(90, 10, 165, 115,width=1,fill="#808080") 
-can.create_rectangle(90, 120, 165, 225,width=1,fill="#808080") 
-can.create_rectangle(90, 230, 165, 335,width=1,fill="#808080") 
+can.create_rectangle(110, 10, 185, 115,width=1,fill="#808080") 
+can.create_rectangle(110, 120, 185, 225,width=1,fill="#808080") 
+can.create_rectangle(110, 230, 185, 335,width=1,fill="#808080") 
 
-can.create_rectangle(170, 10, 245, 115,width=1,fill="#808080") 
-can.create_rectangle(170, 120, 245, 225,width=1,fill="#808080") 
-can.create_rectangle(170, 230, 245, 335,width=1,fill="#808080") 
+can.create_rectangle(190, 10, 265, 115,width=1,fill="#808080") 
+can.create_rectangle(190, 120, 265, 225,width=1,fill="#808080") 
+can.create_rectangle(190, 230, 265, 335,width=1,fill="#808080") 
 
-can.create_rectangle(250, 10, 325, 115,width=1,fill="#808080") 
-can.create_rectangle(250, 120, 325, 225,width=1,fill="#808080") 
-can.create_rectangle(250, 230, 325, 335,width=1,fill="#808080") 
+can.create_rectangle(270, 10, 345, 115,width=1,fill="#808080") 
+can.create_rectangle(270, 120, 345, 225,width=1,fill="#808080") 
+can.create_rectangle(270, 230, 345, 335,width=1,fill="#808080") 
+
+
+# cartes J2 (haut droite)
+can.create_rectangle(1010, 10, 1085, 115,width=1,fill="#808080") 
+can.create_rectangle(1010, 120, 1085, 225,width=1,fill="#808080") 
+can.create_rectangle(1010, 230, 1085, 335,width=1,fill="#808080") 
+
+can.create_rectangle(1090, 10, 1165, 115,width=1,fill="#808080") 
+can.create_rectangle(1090, 120, 1165, 225,width=1,fill="#808080") 
+can.create_rectangle(1090, 230, 1165, 335,width=1,fill="#808080") 
+
+can.create_rectangle(1170, 10, 1245, 115,width=1,fill="#808080") 
+can.create_rectangle(1170, 120, 1245, 225,width=1,fill="#808080") 
+can.create_rectangle(1170, 230, 1245, 335,width=1,fill="#808080") 
+
+can.create_rectangle(1250, 10, 1325, 115,width=1,fill="#808080") 
+can.create_rectangle(1250, 120, 1325, 225,width=1,fill="#808080") 
+can.create_rectangle(1250, 230, 1325, 335,width=1,fill="#808080") 
+
+
+# cartes J3 (bas droite)
+can.create_rectangle(1010, 370, 1085, 475,width=1,fill="#808080") 
+can.create_rectangle(1010, 480, 1085, 585,width=1,fill="#808080") 
+can.create_rectangle(1010, 590, 1085, 695,width=1,fill="#808080") 
+
+can.create_rectangle(1090, 370, 1165, 475,width=1,fill="#808080") 
+can.create_rectangle(1090, 480, 1165, 585,width=1,fill="#808080") 
+can.create_rectangle(1090, 590, 1165, 695,width=1,fill="#808080") 
+
+can.create_rectangle(1170, 370, 1245, 475,width=1,fill="#808080") 
+can.create_rectangle(1170, 480, 1245, 585,width=1,fill="#808080") 
+can.create_rectangle(1170, 590, 1245, 695,width=1,fill="#808080") 
+
+can.create_rectangle(1250, 370, 1325, 475,width=1,fill="#808080") 
+can.create_rectangle(1250, 480, 1325, 585,width=1,fill="#808080") 
+can.create_rectangle(1250, 590, 1325, 695,width=1,fill="#808080") 
+
+
+# cartes J4 (bas gauche)
+can.create_rectangle(30, 370, 105, 475,width=1,fill="#808080") 
+can.create_rectangle(30, 480, 105, 585,width=1,fill="#808080") 
+can.create_rectangle(30, 590, 105, 695,width=1,fill="#808080") 
+
+can.create_rectangle(110, 370, 185, 475,width=1,fill="#808080") 
+can.create_rectangle(110, 480, 185, 585,width=1,fill="#808080") 
+can.create_rectangle(110, 590, 185, 695,width=1,fill="#808080") 
+
+can.create_rectangle(190, 370, 265, 475,width=1,fill="#808080") 
+can.create_rectangle(190, 480, 265, 585,width=1,fill="#808080") 
+can.create_rectangle(190, 590, 265, 695,width=1,fill="#808080") 
+
+can.create_rectangle(270, 370, 345, 475,width=1,fill="#808080") 
+can.create_rectangle(270, 480, 345, 585,width=1,fill="#808080") 
+can.create_rectangle(270, 590, 345, 695,width=1,fill="#808080") 
 
 
 
